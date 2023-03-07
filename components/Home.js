42052efe9f9f311ca50e1b9748b946e302034aec
Alon Landmann <rafael.landmann@gmail.com>
2023-03-07 19:53:28 +0100

    [add] authentication

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,20 @@
+import { useUser } from '@auth0/nextjs-auth0/client'
+
 export default function Home() {
+  const { user, error, isLoading } = useUser()
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
   return (
     <div>
-      Hello world!
+      <h1>Hello {user ? user.name : 'Visitor'}</h1>
+      {!user &&
+        <button onClick={() => { location.replace('/api/auth/login') }}>Login</button>
+      }
+      {user &&
+        <button onClick={() => { location.replace('/api/auth/logout') }}>Logout</button>
+      }
     </div>
   )
 }
